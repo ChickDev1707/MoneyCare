fd20f48e6e322f7f9c1e80afbccaa021d12fb528
thanhdancodoc <71069971+thanhdancodoc@users.noreply.github.com>
2022-06-10 23:11:00 +0700

    design UI and databinding

--- app/src/main/java/com/example/moneycare/ui/view/report/ReportFragment.java
+++ app/src/main/java/com/example/moneycare/ui/view/report/ReportFragment.java
@@ -1,14 +1,42 @@
 package com.example.moneycare.ui.view.report;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.moneycare.R;
+import com.example.moneycare.data.model.Wallet;
+import com.example.moneycare.databinding.FragmentReportBinding;
+import com.example.moneycare.ui.view.MainActivity;
+import com.example.moneycare.ui.view.report.ReportFragment;
+import com.example.moneycare.ui.viewmodel.report.ReportViewModel;
+import com.example.moneycare.utils.DateUtil;
+import com.example.moneycare.utils.ImageUtil;
+import com.example.moneycare.utils.appenum.TransactionTimeFrame;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.whiteelephant.monthpicker.MonthPickerDialog;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,33 +45,27 @@ import com.example.moneycare.R;
  */
 public class ReportFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ReportFragment() {
-        // Required empty public constructor
-    }
+    // TODO: Customize parameter argument names
+    private static final String                         ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private              ReportViewModel           viewModel;
+    private              FragmentReportBinding binding;
+    private              TransactionTimeFrame           timeFrameMode;
+    private              Date                           selectedDate;
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReportFragment.
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
      */
-    // TODO: Rename and change types and number of parameters
-    public static ReportFragment newInstance(String param1, String param2) {
+    public ReportFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static ReportFragment newInstance(int columnCount) {
         ReportFragment fragment = new ReportFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +73,189 @@ public class ReportFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+
+
+//        View view = inflater.inflate(R.layout.fragment_transaction_list, container, false);
+        viewModel = new ViewModelProvider(this).get(ReportViewModel.class);
+        binding = FragmentReportBinding.inflate(getLayoutInflater());
+        binding.setReportListVM(viewModel);
+        binding.setLifecycleOwner(this);
+        timeFrameMode = TransactionTimeFrame.DAY;
+
+        Toolbar toolbar = binding.getRoot().findViewById(R.id.top_app_bar);
+        toolbar.setTitle("");
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        initTransactionSetting();
+        initTransList();
+        initOpenWalletListBtn();
+//        initWalletFromPreference();
+
+        return binding.getRoot();
+    }
+    private void initTransList(){
+        selectedDate = new Date();
+        showTransList();
+    }
+    public void showTransList(){
+//        RecyclerView transList = binding.groupTransactionListTemplate;
+        viewModel.setTransactionUI(timeFrameMode, selectedDate , groupTransactionList -> {
+//            transList.setAdapter(new GroupTransactionRecyclerViewAdapter(groupTransactionList));
+            viewModel.initMoneyInAndOut(groupTransactionList);
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        inflater.inflate(R.menu.top_app_bar, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull @NotNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.date:
+                openPickDateDialog();
+                return true;
+            case R.id.time_frame_day:
+                handleSelectTimeFrame(TransactionTimeFrame.DAY);
+                return true;
+            case R.id.time_frame_month:
+                handleSelectTimeFrame(TransactionTimeFrame.MONTH);
+                return true;
+            case R.id.time_frame_year:
+                handleSelectTimeFrame(TransactionTimeFrame.YEAR);
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void handleSelectTimeFrame(TransactionTimeFrame timeFrame){
+        timeFrameMode = timeFrame;
+        saveTransactionSetting();
+        initTransList();
+    }
+    private void openPickDateDialog(){
+        switch (timeFrameMode){
+            case DAY:
+                showDatePicker();
+                break;
+            case MONTH:
+                showMonthPicker();
+                break;
+            case YEAR:
+                showYearPicker();
+                break;
+        }
+    }
+    private void showDatePicker(){
+        Long today = MaterialDatePicker.todayInUtcMilliseconds();
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        MaterialDatePicker datePicker = builder
+                .setTitleText("Chọn khung thời gian")
+                .setSelection(today)
+                .build();
+        datePicker.show(getParentFragmentManager(), "DATE_PICKER");
+
+        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                selectedDate = new Date((Long) selection);
+                showTransList();
+            }
+        });
+    }
+    private void showMonthPicker(){
+        Calendar today = Calendar.getInstance();
+        MonthPickerDialog.Builder builder = new MonthPickerDialog.Builder(getContext(), new MonthPickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(int selectedMonth, int selectedYear) {
+                selectedDate = DateUtil.createDate(1, selectedMonth, selectedYear);
+                showTransList();
+            }
+        }, today.get(Calendar.YEAR), today.get(Calendar.MONTH));
+        builder.setActivatedMonth(Calendar.JULY)
+                .setMinYear(1990)
+                .setActivatedYear(2022)
+                .setMaxYear(2030)
+                .setTitle("Select month")
+                .build()
+                .show();
+    }
+    private  void showYearPicker(){
+        Calendar today = Calendar.getInstance();
+        MonthPickerDialog.Builder builder = new MonthPickerDialog.Builder(getContext(), new MonthPickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(int selectedMonth, int selectedYear) {
+                selectedDate = DateUtil.createDate(1, selectedMonth, selectedYear);
+                showTransList();
+            }
+        }, today.get(Calendar.YEAR), today.get(Calendar.MONTH));
+        builder.setActivatedMonth(Calendar.JULY)
+                .setMinYear(1990)
+                .setActivatedYear(2022)
+                .setMaxYear(2030)
+                .setTitle("Select year")
+                .showYearOnly()
+                .build()
+                .show();
+    }
+
+    private void saveTransactionSetting(){
+        SharedPreferences sharedPref = getActivity().getSharedPreferences(getString(R.string.transaction_preference_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(getString(R.string.time_frame_key), timeFrameMode.getValue());
+        editor.apply();
+    }
+    public void initTransactionSetting(){
+        SharedPreferences sharedPref = getActivity().getSharedPreferences(getString(R.string.transaction_preference_key), Context.MODE_PRIVATE);
+        int timeFrameValue = sharedPref.getInt(getString(R.string.time_frame_key), 1);
+        timeFrameMode = TransactionTimeFrame.getTimeFrame(timeFrameValue);
+    }
+    private void initOpenWalletListBtn(){
+        binding.mainAppBar.walletIconBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent toSelectWalletIntent = new Intent(getActivity(), SelectWalletActivity.class);
+//                startActivity(toSelectWalletIntent);
+                ((MainActivity) getActivity()).launchReloadWallet();
+            }
+        });
+    }
+//    public void initWalletFromPreference(){
+//        SharedPreferences sharedPref = getActivity().getSharedPreferences(getString(R.string.transaction_preference_key), Context.MODE_PRIVATE);
+//        String walletId = sharedPref.getString(getString(R.string.current_wallet_key), "");
+//
+//        if(walletId == ""){
+//            // no pref
+//            viewModel.fetchFirstWallet(wallet->{
+//                initWallet(wallet);
+//                saveWalletPreference(wallet);
+//            });
+//        }else{
+//            viewModel.fetchWallet(walletId, this::initWallet);
+//        }
+//    }
+    private void initWallet(Wallet wallet){
+        binding.mainAppBar.walletName.setText(wallet.name);
+        binding.mainAppBar.walletMoney.setText(Long.toString(wallet.money));
+
+        if(wallet.image!= ""){
+            Bitmap walletBimapImg = ImageUtil.toBitmap(wallet.image);
+            BitmapDrawable walletBitmapDrawable = new BitmapDrawable(getResources(), walletBimapImg);
+            binding.mainAppBar.walletIconBtn.setBackground(walletBitmapDrawable);
+        }
+    }
+    private void saveWalletPreference(Wallet wallet){
+        SharedPreferences sharedPref = getActivity().getSharedPreferences(getString(R.string.transaction_preference_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.current_wallet_key), wallet.id);
+        editor.apply();
     }
 }
