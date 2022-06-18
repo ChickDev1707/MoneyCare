b25c63f359dd9ff964cbd664ce01fa180e13c0bd
ChickDev1707 <thuancoixy234786@gmail.com>
2022-06-18 15:20:58 +0700

    add toast util

--- app/src/main/java/com/example/moneycare/utils/ToastUtil.java
+++ app/src/main/java/com/example/moneycare/utils/ToastUtil.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 import com.example.moneycare.ui.view.transaction.trans.NewTransactionActivity;
 
 public class ToastUtil {
-    public void showToast(Context context, String message){
+    public static void showToast(Context context, String message){
         Toast toast =  Toast.makeText(context, message, Toast.LENGTH_SHORT);
         toast.show();
     }
